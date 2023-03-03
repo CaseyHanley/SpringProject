06481ec721fd1f5342912f73a78137589dcc90d0
Casey.T.Hanley <caseythomashanley@hotmail.com>
2023-03-03 09:16:48 -0600

    Finsihed Annotaion side

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -14,9 +15,16 @@ public class App
 
         ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
 
+        AnnotationConfigApplicationContext  context2 = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        student student2 = context.getBean("student", student.class);
+
+
         student student1 = (student) context.getBean("student");
 
         System.out.println(student1);
+        System.out.println(student2);
+
 
 
     }
